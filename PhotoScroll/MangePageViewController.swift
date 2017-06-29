0b593861e183a707e4144874afd72f62519173ad
neal <caoyahua@cloudoc.cn>
2017-06-29 17:47:33 +0800

    Add pageController

--- PhotoScroll/MangePageViewController.swift
+++ PhotoScroll/MangePageViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class MangePageViewController: UIPageViewController {
 
+    var photos = ["photo1","photo2","photo3","photo4","photo5"]
+    var currentIndex : Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dataSource = self
         // Do any additional setup after loading the view.
+        
+        if let viewController = viewPhotoCommentController(index: currentIndex ?? 0) {
+            let viewControllers = [viewController]
+            setViewControllers(viewControllers, direction: .forward, animated: false, completion: nil)
+            
+        }
     }
 
+    fileprivate func viewPhotoCommentController(index:Int)-> PhotoCommentViewController? {
+        if let storyboard = storyboard,
+            let page = storyboard.instantiateViewController(withIdentifier:"PhotoCommentViewController") as? PhotoCommentViewController
+        {
+            page.photoName = photos[index]
+            page.photoIndex = index
+            return page
+        }
+        return nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,3 +54,41 @@ class MangePageViewController: UIPageViewController {
     */
 
 }
+
+extension MangePageViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let viewController = viewController as? PhotoCommentViewController {
+            guard let index = viewController.photoIndex,
+            index != photos.count - 1 else {
+                return nil
+            }
+            return viewPhotoCommentController(index: index + 1)
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let viewController = viewController as? PhotoCommentViewController  {
+            guard let index = viewController.photoIndex,
+                index != 0 else {
+                    return nil
+            }
+            return viewPhotoCommentController(index: index - 1)
+        }
+        return nil
+    }
+    
+    // MARK: pageControl
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return photos.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return currentIndex ?? 0
+    }
+    
+}
+
+
--- PhotoScroll/ZoomPhotoViewController.swift
+++ PhotoScroll/ZoomPhotoViewController.swift
@@ -10,12 +10,54 @@ import UIKit
 
 class ZoomPhotoViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingContraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    
+    var photoName : String!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: photoName)
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        updateMinZoomScale(forSize: view.bounds.size)
+    }
+    
+    
+    fileprivate func updateMinZoomScale(forSize size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.width / imageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = minScale
+        
+    }
+    
+    
+    fileprivate func updateConstraints(forSize size: CGSize) {
+        let yOffset = max(0, (size.height - imageView.frame.height)/2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, (size.width - imageView.frame.width)/2)
+        imageViewLeadingContraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,4 +74,15 @@ class ZoomPhotoViewController: UIViewController {
     }
     */
 
+}
+
+extension ZoomPhotoViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        updateConstraints(forSize: view.bounds.size)
+    }
 }
